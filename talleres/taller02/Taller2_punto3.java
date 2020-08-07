7b5d54c46d62fcc1147f73c1f57c4c17adb689e2
spuertaf <68928400+spuertaf@users.noreply.github.com>
2020-08-07 18:15:21 -0500

    Add files via upload

---
+++ talleres/taller02/Taller2_punto3.java
@@ -0,0 +1,15 @@
+class Main {
+
+  public static void combinations(String s){
+    combinationsAux("",s);
+  }
+
+  public static void combinationsAux (String prefix, String s){
+    if (s.length()==0){
+      System.out.println(prefix);
+    } else {
+      combinationsAux(prefix, s.substring(1));
+      combinationsAux(prefix + s.charAt(0), s.substring(1));
+    }
+  }
+}
