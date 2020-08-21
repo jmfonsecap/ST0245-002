de4469e452c8a9998ad2b058b2643fe8b0fd8fdb
jmfonsecap <68928395+jmfonsecap@users.noreply.github.com>
2020-08-20 19:50:50 -0500

    Add files via upload

--- laboratorios/lab01/codigo/ADN.java
+++ laboratorios/lab01/codigo/ADN.java
@@ -1,11 +1,23 @@
-import java.io.*;
 public class ADN{
     
+    /**
+     * Metodo que se encarga de calcular la cadena comun más larga entre dos strings.
+     * @param string1 Primer string
+     * @param string2 Segundo string
+     * @return tamaño de la cadena comun más larga.
+     */
     public static int lcsDNA(String string1, String string2){
         return lcsDNAAux(string1,string2,string1.length(),string2.length());
         
     }
-
+    /**
+     * Metodo auxiliar para lcsDNA
+     * @param string1 Primer string
+     * @param string2 Segundo string
+     * @param m tamaño de string 1
+     * @param n tamaño de string 2
+     * @return tamaño de la cadena comun más larga.
+     */
     private static int lcsDNAAux(String string1, String string2,int m,int n) {
         if (m<=0||n<=0)
         {
@@ -16,11 +28,5 @@ public class ADN{
             return 1 + lcsDNAAux(string1.substring(0,m-1),string2.substring(0,n-1),string1.substring(0,m-1).length(),string2.substring(0,n-1).length());
         }
         return Math.max(lcsDNAAux(string1.substring(0,m-1),string2,string1.substring(0,m-1).length(),n), lcsDNAAux(string1,string2.substring(0,n-1),m,string2.substring(0,n-1).length()));
-    }
-    
-    public static void main(String[] args)throws IOException {
-        
-        int i= lcsDNA("axyt", "ayzx");
-        System.out.println(i);
     }
 }
